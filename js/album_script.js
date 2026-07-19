141c90f11b25eaa8299a723445cd609fca360994
joseph07xx <josephdubonfree@gmail.com>
2026-07-19 16:37:17 -0600

    Update album_script.js

--- js/album_script.js
+++ js/album_script.js
@@ -1,3 +1,20 @@
+(function crearParticulas() {
+            const contenedor = document.getElementById('particulas');
+            const cant = 35;
+            for (let i = 0; i < cant; i++) {
+                const p = document.createElement('div');
+                p.classList.add('particula');
+                const size = Math.random() * 60 + 20;
+                p.style.width = size + 'px';
+                p.style.height = size + 'px';
+                p.style.left = Math.random() * 100 + '%';
+                p.style.animationDuration = (Math.random() * 25 + 20) + 's';
+                p.style.animationDelay = (Math.random() * 20) + 's';
+                p.style.opacity = Math.random() * 0.2 + 0.05;
+                contenedor.appendChild(p);
+            }
+        })();
+
 const misRecuerdos = [
             { id: "1pm2svUFVCKT20qZb9E367L_oBgRJZFx-", titulo: "Nuestro comienzo", fecha: "2023" },
             { id: "1ImtKk7UWGNKHqpLvwVlCd5NrW99pgFuj", titulo: "Mirada que enamora", fecha: "Agosto 2023" },
